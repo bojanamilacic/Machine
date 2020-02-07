6dc44db1c5dc5be40a73f0d02af748796ab065e3
Bojana <bojana.milacic97@gmail.com>
2020-02-07 15:28:56 +0100

    Change value of coins

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,40 +6,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  paymantAmount: number;
+  paymantAmount: number = 0;
   insertCoin: number = 0;
   change: any;
   pay: number;
-  sixthCoin: number;
-  fifthCoin: number;
-  fourthCoin: number;
-  thirdCoin: number;
-  secondCoin: number;
-  firstCoin: number;
-  showChange: string;
+  ninthCoin: number = 0;
+  eightCoin: number = 0;
+  seventhCoin: number = 0;
+  sixthCoin: number = 0;
+  fifthCoin: number = 0;
+  fourthCoin: number = 0;
+  thirdCoin: number = 0;
+  secondCoin: number = 0;
+  firstCoin: number = 0;
+  showChange: any;
   inputValue: number;
   inStockCoins: any;
-  sixthCoinInStock: number;
-  fifthCoinInStock: number;
-  fourthCoinInStock: number;
-  thirdCoinInStock: number;
-  secondCoinInStock: number;
-  firstCoinInStock: number;
+  ninthCoinInStock: number = 0;
+  eightCoinInStock: number = 0;
+  seventhCoinInStock: number = 0;
+  sixthCoinInStock: number = 0;
+  fifthCoinInStock: number = 0;
+  fourthCoinInStock: number = 0;
+  thirdCoinInStock: number = 0;
+  secondCoinInStock: number = 0;
+  firstCoinInStock: number = 0;
 
   constructor() { }
 
   showCoins(){
-    this.inStockCoins = "Broj kovanica u automatu: " + this.sixthCoinInStock + " od 5 kovanica," + this.fifthCoinInStock + " od 2 kovanica,  " +
-        this.fourthCoinInStock + " od 1 kovanica,  " + this.thirdCoinInStock + " od 0.5 kovanica,  " +
-        this.secondCoinInStock + " od 0.2 kovanica,  " + this.firstCoinInStock + " od 0.1 kovanica"
+    this.inStockCoins = "Broj novca u automatu: " +this.ninthCoinInStock + " od 50 kovanica," +
+       this.eightCoinInStock +  " od 20 kovanica,  " + this.seventhCoinInStock + " od 10 kovanica," +
+      this.sixthCoinInStock + " od 5 kovanica," + this.fifthCoinInStock + " od 2 kovanica,  " +
+      this.fourthCoinInStock + " od 1 kovanica,  " + this.thirdCoinInStock + " od 0.5 kovanica,  " +
+      this.secondCoinInStock + " od 0.2 kovanica,  " + this.firstCoinInStock + " od 0.1 kovanica"
     if(this.sixthCoinInStock == 0 && this.fifthCoinInStock ==0 && this.fourthCoinInStock==0 && 
       this.thirdCoinInStock==0 && this.secondCoinInStock==0 && this.firstCoinInStock ==0)
       this.showChange = "Automat nema kovanica";
     }
   onItemSelector() {
     if (this.inputValue == 0.1 || this.inputValue == 0.2 || this.inputValue == 0.5
-      || this.inputValue == 1 || this.inputValue == 2 || this.inputValue == 5) {
-      if (this.inputValue == 5) {
+      || this.inputValue == 1 || this.inputValue == 2 || this.inputValue == 5 || 
+      this.inputValue == 10 || this.inputValue == 20 || this.inputValue == 50) {
+        if (this.inputValue == 50) {
+          this.ninthCoinInStock += 1;
+        }
+        if (this.inputValue == 20) {
+          this.eightCoinInStock += 1;
+        }
+        if (this.inputValue == 10) {
+          this.seventhCoinInStock += 1;
+        }
+        if (this.inputValue == 5) {
         this.sixthCoinInStock += 1;
       }
       if (this.inputValue == 2) {
@@ -60,7 +78,9 @@ export class HomeComponent implements OnInit {
       this.insertCoin += this.inputValue;
       this.inputValue = 0;
       this.getPay();
-      this.inStockCoins = "Broj kovanica u automatu: " + this.sixthCoinInStock + " od 5 kovanica," + this.fifthCoinInStock + " od 2 kovanica,  " +
+      this.inStockCoins = "Broj novca u automatu: " +this.ninthCoinInStock + " od 50 kovanica," +
+       this.eightCoinInStock +  " od 20 kovanica,  " + this.seventhCoinInStock + " od 10 kovanica," +
+      this.sixthCoinInStock + " od 5 kovanica," + this.fifthCoinInStock + " od 2 kovanica,  " +
       this.fourthCoinInStock + " od 1 kovanica,  " + this.thirdCoinInStock + " od 0.5 kovanica,  " +
       this.secondCoinInStock + " od 0.2 kovanica,  " + this.firstCoinInStock + " od 0.1 kovanica"
       //  The status of the coins in the machine is shown just for example
@@ -86,6 +106,40 @@ export class HomeComponent implements OnInit {
   calculateChange() {
     this.pay = this.insertCoin - this.paymantAmount;
     this.change = this.pay * 10;
+
+    if (this.ninthCoinInStock != 0) {
+      this.ninthCoin = Math.floor(this.change / (50 * 10));
+      if (this.ninthCoin > this.ninthCoinInStock) {
+        this.ninthCoin = this.ninthCoinInStock;
+        this.ninthCoinInStock = 0;
+      }
+      else {
+        this.ninthCoinInStock = this.ninthCoinInStock - this.ninthCoin;
+      }
+      this.change = Math.floor(this.change - (this.ninthCoin * 50 * 10));
+    }
+    if (this.eightCoinInStock != 0) {
+      this.eightCoin = Math.floor(this.change / (20 * 10));
+      if (this.eightCoin > this.eightCoinInStock) {
+        this.eightCoin = this.eightCoinInStock;
+        this.eightCoinInStock = 0;
+      }
+      else {
+        this.eightCoinInStock = this.eightCoinInStock - this.eightCoin;
+      }
+      this.change = Math.floor(this.change - (this.eightCoin * 20 * 10));
+    }
+    if (this.seventhCoinInStock != 0) {
+      this.seventhCoin = Math.floor(this.change / (10 * 10));
+      if (this.seventhCoin > this.seventhCoinInStock) {
+        this.seventhCoin = this.seventhCoinInStock;
+        this.seventhCoinInStock = 0;
+      }
+      else {
+        this.seventhCoinInStock = this.seventhCoinInStock - this.seventhCoin;
+      }
+      this.change = Math.floor(this.change - (this.seventhCoin * 10 * 10));
+    }
     if (this.sixthCoinInStock != 0) {
       this.sixthCoin = Math.floor(this.change / (5 * 10));
       if (this.sixthCoin > this.sixthCoinInStock) {
@@ -154,16 +208,22 @@ export class HomeComponent implements OnInit {
       }
       else {
         this.firstCoinInStock = this.firstCoinInStock - this.firstCoin;
+        this.change = Math.floor(this.change - (this.firstCoin * 0.1 * 10));
       }
     }
      
 
-    this.showChange = 'Kusur iznosi: ' + this.firstCoin + ' od ' + 0.1 + ' , '
+    this.showChange = 'Kusur iznosi: ' 
+      
+      + this.firstCoin + ' od ' + 0.1 + ' , '
       + this.secondCoin + ' od ' + 0.2 + ' , '
       + this.thirdCoin + ' od ' + 0.5 + ' , '
       + this.fourthCoin + ' od ' + 1 + ' , '
       + this.fifthCoin + ' od ' + 2 + ' , '
-      + this.sixthCoin + ' od ' + 5;
+      + this.sixthCoin + ' od ' + 5 + ' , '
+      + this.seventhCoin + ' od ' + 10 + ' , '
+      + this.eightCoin + ' od ' + 20 + ' , '
+      + this.ninthCoin + ' od ' + 50;
     (document.getElementById('disabledBtn') as HTMLInputElement).disabled = true;
 
     setTimeout(() => {
